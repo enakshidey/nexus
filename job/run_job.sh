efd9948216b0c4d4ce1bdcdfbd7fe9e015b6e07a
Enakshi <exd4668@mavs.uta.edu>
2023-09-29 14:30:53 -0500

    corrected nexus IC bash script

--- job/run_job.sh
+++ job/run_job.sh
@@ -1,1 +1,15 @@
-sbatch --array=1-2 Enakshi_Nexus.sh
+#!/bin/bash
+
+# Declare an array of string with type, these are the EL drift vel values to run
+PRESSURE=10
+GAS=enrichedXe
+
+# Iterate the string array using for loop
+mkdir -p Pressure_${PRESSURE}bar/gas_$GAS/${N_EVENTS}k/${ENERGY}MeV/${STEP_LENGTH}mm
+cd  Pressure_${PRESSURE}bar/gas_$GAS/${N_EVENTS}k/${ENERGY}MeV/${STEP_LENGTH}mm
+
+
+cp ../Enakshi_Nexus.sh .
+sed -i "s#.*SBATCH -J.*#\#SBATCH -J NEW_MC208_${ELDrift}ELDrift \# A single job name for the array#" Enakshi_Nexus.sh
+ sbatch --array=1-500 Enakshi_Nexus.sh
+cd ..

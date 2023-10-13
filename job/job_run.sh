0be16040caa0b990577351cb39532d4337d4a371
Enakshi <exd4668@mavs.uta.edu>
2023-10-13 09:51:34 -0500

    New run file

---
+++ job/job_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Declare an array of string with type, these are the EL drift vel values to run
+PRESSURE=15
+GAS=enrichedXe
+STEP_LENGTH=0.1
+ENERGY=2
+N_EVENTS=20
+
+# Iterate the string array using for loop
+mkdir -p Pressure_${PRESSURE}bar/gas_${GAS}/${N_EVENTS}k/${ENERGY}MeV/${STEP_LENGTH}mm
+cd  Pressure_${PRESSURE}bar/gas_${GAS}/${N_EVENTS}k/${ENERGY}MeV/${STEP_LENGTH}mm
+
+
+cp ../../../../../Enakshi_Nexus.sh .
+
+sed -i "s#.*STEP_LENGTH=.*#STEP_LENGTH=${STEP_LENGTH} #" Enakshi_Nexus.sh
+sed -i "s#.*PRESSURE=.*#PRESSURE=${PRESSURE} #" Enakshi_Nexus.sh
+sed -i "s#.*ENERGY=.*#ENERGY=${ENERGY}#" Enakshi_Nexus.sh
+sed -i "s#.*GAS=.*#GAS=${GAS}#" Enakshi_Nexus.sh 
+sed -i "s#.*N_EVENTS=.*#N_EVENTS=${N_EVENTS} #" Enakshi_Nexus.sh 
+
+sbatch --array=1-40 Enakshi_Nexus.sh
